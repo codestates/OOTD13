a400892f16150968fe02c0d3fc34b1203fe761ee
qwp0905 <qwp0905@gmail.com>
2022-01-21 21:36:58 +0900

    Add findPost,createPost modules

---
+++ server/modules/findPost.js
@@ -0,0 +1,9 @@
+const models=require('../models')
+
+module.exports={
+    findPost: async (postId)=>{
+        const article = await models.article.findOne({where:{id:postId}});
+        const content = await models.content.findOne({where:{id:article.content_id}});
+        return content;
+    }
+}
