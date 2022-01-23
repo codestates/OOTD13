fe4b62a8410a67511b0f9224835d2a0c531c5ed3
qwp0905 <qwp0905@gmail.com>
2022-01-24 00:41:02 +0900

    Add get /post endpoint

--- server/modules/findPost.js
+++ server/modules/findPost.js
@@ -1,7 +1,11 @@
 const models=require('../models')
 
 module.exports=async (postId)=>{
-    const article = await models.article.findOne({where:{id:postId}});
-    const content = await models.content.findOne({where:{id:article.content_id}});
-    return content;
+    try{
+        const article = await models.article.findOne({where:{id:postId}});
+        const content = await models.content.findOne({where:{id:article.content_id}});
+        return content;
+    } catch (err){
+        return null;
+    }
 }
