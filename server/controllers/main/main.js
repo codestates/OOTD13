bf993c1998bf6e64ab9286e0a6aa8162c678cf0c
qwp0905 <qwp0905@gmail.com>
2022-01-23 18:28:44 +0900

    Fix main module filtering

--- server/controllers/main/main.js
+++ server/controllers/main/main.js
@@ -5,39 +5,40 @@ module.exports ={
         const order=req.query.order||'recent';
         const page=req.query.page||1;
         let articles=await models.article.findAll();
+        let articleIds=articles.map(el=>el.dataValues.id)
         
         if(req.query.sex){
             const tag= await models.tag.findOne({where:{tag_num:100+req.query.sex}});
-            let articleIds=await models.article_tag.findAll({where:{tag_id:tag.dataValues.id}});
-            articleIds=articleIds.map(el=>el.dataValues.article_id)
-            articles=await Promise.all(articleIds.map(async(id)=>{
-                return await models.article.findOne({where:{id:id}})
-            }))
+            let filteringIds=await models.article_tag.findAll({where:{tag_id:tag.dataValues.id}});
+            filteringIds=filteringIds.map(el=>el.dataValues.article_id);
+            articleIds=articleIds.filter(el=>filteringIds.includes(el))
         }
         if(req.query.weather){
             const tag= await models.tag.findOne({where:{tag_num:200+req.query.weather}});
-            let articleIds=await models.article_tag.findAll({where:{tag_id:tag.dataValues.id}});
-            articleIds=articleIds.map(el=>el.dataValues.article_id)
-            articles=await Promise.all(articleIds.map(async(id)=>{
-                return await models.article.findOne({where:{id:id}})
-            }))
+            let filteringIds=await models.article_tag.findAll({where:{tag_id:tag.dataValues.id}});
+            filteringIds=filteringIds.map(el=>el.dataValues.article_id);
+            articleIds=articleIds.filter(el=>filteringIds.includes(el))
         }
         if(req.query.season){
             const tag= await models.tag.findOne({where:{tag_num:300+req.query.season}});
-            let articleIds=await models.article_tag.findAll({where:{tag_id:tag.dataValues.id}});
-            articleIds=articleIds.map(el=>el.dataValues.article_id)
-            articles=await Promise.all(articleIds.map(async(id)=>{
-                return await models.article.findOne({where:{id:id}})
-            }))
+            let filteringIds=await models.article_tag.findAll({where:{tag_id:tag.dataValues.id}});
+            filteringIds=filteringIds.map(el=>el.dataValues.article_id);
+            articleIds=articleIds.filter(el=>filteringIds.includes(el))
         }
         if(req.query.style){
             const tag= await models.tag.findOne({where:{tag_num:400+req.query.style}});
-            let articleIds=await models.article_tag.findAll({where:{tag_id:tag.dataValues.id}});
-            articleIds=articleIds.map(el=>el.dataValues.article_id)
-            articles=await Promise.all(articleIds.map(async(id)=>{
-                return await models.article.findOne({where:{id:id}})
-            }))
+            let filteringIds=await models.article_tag.findAll({where:{tag_id:tag.dataValues.id}});
+            filteringIds=filteringIds.map(el=>el.dataValues.article_id);
+            articleIds=articleIds.filter(el=>filteringIds.includes(el))
         }
+
+        if(articleIds.length){
+            res.status(400).send({response:'not found'})
+        }
+
+        articles=await Promise.all(articleIds.map(async (id)=>{
+            return await models.article.findOne({where:{id:id}})
+        }))
 
         const sorting={recent:'createdAt',popular:'view',like:'like',old:'createdAt'}
         articles.sort((a,b)=>{
@@ -75,4 +76,3 @@ module.exports ={
         })
     }
 }
-//서버 요청방식에 대한 논의 필요
