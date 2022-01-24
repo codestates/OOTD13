8528cefeb63a6b4fe366bb7c574ed0ff2e827f7f
qwp0905 <qwp0905@gmail.com>
2022-01-24 15:01:09 +0900

    Fix defaultValue

--- server/modules/createUser.js
+++ server/modules/createUser.js
@@ -1,20 +1,18 @@
 const models = require("../models");
 
 module.exports = {
-  createUser: async (loginMethod, email, username, password) => {
+  createUser: (loginMethod, email, username, password) => {
     if (!loginMethod) {
       models.user.create({
         email: email,
         username: username,
         password: password,
-        login_method: null,
       });
     }
     models.user.create({
       email: email,
       login_method: loginMethod,
       username: username,
-      password: null,
     });
   },
 };
