6aa939d104f18eb65105404b173ca555556849f2
Jhin3283 <hj3283@gmail.com>
2022-01-24 16:49:49 +0900

    Fix githubLogin

--- server/controllers/user/githubLogin.js
+++ server/controllers/user/githubLogin.js
@@ -1,7 +1,40 @@
-const createUser = require('./../../modules/createUser')
-const requestTokenGit = require('./../../modules/requestTokenGit')
-const requestUserInfoGit = require('./../../modules/requestUserInfoGit')
+const createUser = require("./../../modules/createUser");
+const requestTokenGit = require("./../../modules/requestTokenGit");
+const requestUserInfoGit = require("./../../modules/requestUserInfoGit");
+const findUser = require("./../../modules/findUser");
 
-module.exports= async (req,res)=>{
-    requestTokenGit(req.body.authorizationCode)
-}
+module.exports = async (req, res) => {
+  const token = requestTokenGit(req.body.authorizationCode);
+  const gitUserInfo = requestUserInfoGit(token);
+  const userInfo = findUser({ email: (await gitUserInfo).email });
+  if (!token) {
+    return res.status(401).send({ response: "invalid code" });
+  }
+  if (!userInfo) {
+    await createUser({
+      loginMethod: 1,
+      email: (await gitUserInfo).email,
+      username: (await gitUserInfo).username,
+    });
+    return res.status(201).send({
+      response: "created",
+      data: {
+        userInfo: {
+          email: userInfo.email,
+          username: userInfo.username,
+          createdAt: userInfo.createdAt,
+          loginMethod: userInfo.login_method,
+        },
+      },
+    });
+  }
+  return res.status(200).send({
+    response: "ok",
+    data: {
+      email: userInfo.email,
+      username: userInfo.username,
+      createdAt: userInfo.createdAt,
+      loginMethod: userInfo.login_method,
+    },
+  });
+};
