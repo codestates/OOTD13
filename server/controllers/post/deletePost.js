9a54d7e26052ba5f10e54f4131580d0dc1856f91
qwp0905 <qwp0905@gmail.com>
2022-01-25 09:00:35 +0900

    Fix deletePost

--- server/controllers/post/deletePost.js
+++ server/controllers/post/deletePost.js
@@ -3,13 +3,13 @@ const validateToken=require('../../modules/validateToken');
 const findUser=require('../../modules/findUser');
 
 module.exports= async (req,res)=>{
-    // try {
+    try {
         const loginMethod=req.query.loginmethod||0;
         const tokenInfo=await validateToken.validateToken(loginMethod,req.headers.authorization);
         await findUser({loginMethod:loginMethod,email:tokenInfo.email});
         await deletePost(req.query.id);
         res.status(204).send({response:'deleted'})
-    // } catch {
-    //     res.status(401).send({response:'not authorized'})
-    // }
+    } catch {
+        res.status(401).send({response:'not authorized'})
+    }
 }
