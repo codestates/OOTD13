9eb57fd0e4865ce409f831c35328bc2f8d36bda7
Jhin3283 <hj3283@gmail.com>
2022-01-25 18:14:52 +0900

    Fix withDrawal

--- server/controllers/user/withDrawal.js
+++ server/controllers/user/withDrawal.js
@@ -3,6 +3,7 @@ const validateToken = require("./../../modules/validateToken");
 const models = require("./../../models");
 
 module.exports = async (req, res) => {
+  try{
   const userInfo = await validateToken.validateToken(
     req.query.loginmethod,
     req.headers.authorization
@@ -10,21 +11,13 @@ module.exports = async (req, res) => {
   if (!userInfo) return res.status(401).send({ response: "not authorized1" });
   deleteUser
     .deleteUserPost(userInfo.email, req.query.loginmethod)
-    // .then(()=>{
-    //   deleteUser.deleteUser(userInfo.email, req.query.loginmethod)
-    // })
     .then(() => {
       res.status(204).send({ response: "deleted" });
     })
     .catch(async () => {
-      // try {
-      //   const userId = await models.user.findOne({
-      //     where: { email: userInfo.email, login_method: req.query.loginmethod },
-      //   });
-      //   await models.user.destroy({ where: { id: userId.id } });
-      //   return res.status(204).send({ response: "deleted" });
-      // } catch {
         return res.status(401).send({ response: "not authorized2" });
-      //}
     });
+  } catch{
+    return res.status(401).send({ response: "not authorized1" });
+  }
 };
